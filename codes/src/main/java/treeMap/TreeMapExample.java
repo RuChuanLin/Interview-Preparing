30ed8bc3aa45dfdc384f4a95cc42492853c0fcde
RuChuanLin <ruchuanlin@gmail.com>
2022-09-30 22:31:03 +0800

    ij

--- codes/src/main/java/treeMap/TreeMapExample.java
+++ codes/src/main/java/treeMap/TreeMapExample.java
@@ -1,4 +1,21 @@
 package treeMap;
 
+import java.util.TreeMap;
+
 public class TreeMapExample {
+
+  public static void main(String[] args) {
+    TreeMap<Integer, Integer> tm = new TreeMap<>();
+
+    for (int i = 1; i <= 15; i++) {
+      tm.put(i, i);
+    }
+
+    System.out.println(tm);
+  }
+
+  static class Dummy {
+    int num = 0;
+    String str = "1";
+  }
 }
